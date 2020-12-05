28d2b63fe1eb94b6152e529aa57c0b397eefa1d6
Rajpal Singh <srajpal206@gmail.com>
2020-12-05 23:25:03 +0530

    Angular App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'arkenea-test-app';
+  @ViewChild('f', { static: false }) signupForm!: NgForm;
+  user = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: ''
+  };
+  submitted = false;
+  selectedFile!: File;
+
+  // onSubmit(form: NgForm) {
+  //   console.log(form);
+  // }
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    this.user.firstName = this.signupForm.value.firstName;
+    this.user.lastName = this.signupForm.value.lastName;
+    this.user.email = this.signupForm.value.email;
+    this.user.phoneNumber = this.signupForm.value.phoneNumber;
+
+    this.signupForm.reset();
+  }
+
+  onFileSelected(file: any) {
+    console.log(file);
+    this.selectedFile = <File>file.target.files[0];
+  }
+
+  onUpload() {
+    const fd = new FormData;
+    fd.append('image', this.selectedFile, this.selectedFile.name)
+    this.http.post('url_of_firebase_or_any_cloud', fd)
+    .subscribe((res)=>{
+      console.log(res);
+    })
+  }
 }
